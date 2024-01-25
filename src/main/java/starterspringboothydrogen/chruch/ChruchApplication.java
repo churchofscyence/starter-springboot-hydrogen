aa2157e58cf911d670656ba04828d4dc6894d5d9
johnnycowboy3033 <112647925+johnnycowboy3033@users.noreply.github.com>
2024-01-25 11:15:41 -0500

    Add files via upload

---
+++ src/main/java/starterspringboothydrogen/chruch/ChruchApplication.java
@@ -0,0 +1,13 @@
+package starterspringboothydrogen.chruch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChruchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChruchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/starterspringboothydrogen/chruch/ChruchApplicationTests.java
@@ -0,0 +1,13 @@
+package starterspringboothydrogen.chruch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChruchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
